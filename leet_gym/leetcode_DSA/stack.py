24d06d4d4c2f69e6a97eb8c151f330e748bd9a74
unknown <mshossaincse@gmail.com>
2023-05-12 11:38:53 +0600

    expanding the solution by two pointer problem no. 1968

--- leet_gym/leetcode_DSA/stack.py
+++ leet_gym/leetcode_DSA/stack.py
@@ -22,3 +22,20 @@ class Solution:
         return not stack
 
             
+class Solution:
+    def makeGood(self, s: str) -> str:
+        """itype: str
+        rtype: str
+        return a string after operating the given
+        instruction for a given string.
+        """
+        stack = []
+        for idx in range(len(s)):
+            if not stack:
+                stack.append(s[idx])
+                continue
+            if(stack[-1].isupper() and stack[-1].lower() == s[idx]) or (stack[-1].islower() and stack[-1].upper() == s[idx]):
+                stack.pop()
+                continue
+            stack.append(s[idx])
+        return "".join(stack)
--- leet_gym/leetcode_DSA/two-pointer.py
+++ leet_gym/leetcode_DSA/two-pointer.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         """
@@ -12,3 +15,22 @@ class Solution:
             if height[l]>height[r]: r -= 1
             else: l += 1
         return result
+
+#Python solution: 1968-array-with-elements-not-equal-to-average-of-neighbors
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        """itype: List[int]
+        rtype: List[int]
+        checking the average of two adjacent
+        numbers of a number is not equal.
+        """
+        for idx in range(1, len(nums)-1):
+            if (nums[idx-1] > nums[idx] > nums[idx+1]):
+                nums[idx] = nums[idx] ^ nums[idx+1]
+                nums[idx+1] = nums[idx] ^ nums[idx+1]
+                nums[idx] = nums[idx] ^ nums[idx+1]
+            if (nums[idx-1] < nums[idx] < nums[idx+1]):
+                nums[idx] = nums[idx] ^ nums[idx+1]
+                nums[idx+1] = nums[idx] ^ nums[idx+1]
+                nums[idx] = nums[idx] ^ nums[idx+1]
+        return nums
